9d5cff64504f782787ce50aa5d9511150400f9be
michal <michal.nand@gmail.com>
2018-08-14 15:31:20 +0200

    nets

--- examples/classification_test/main.cpp
+++ examples/classification_test/main.cpp
@@ -12,6 +12,7 @@ int main()
 {
   srand(time(NULL));
 
+/*
   {
     DatasetMnist dataset ( "/home/michal/dataset/mnist/train-images.idx3-ubyte",
                            "/home/michal/dataset/mnist/train-labels.idx1-ubyte",
@@ -28,7 +29,7 @@ int main()
       experiment.run();
     }
   }
-
+*/
   {
     DatasetCIFAR10 dataset ( "/home/michal/dataset/cifar_10/data_batch_1.bin",
                              "/home/michal/dataset/cifar_10/data_batch_2.bin",
