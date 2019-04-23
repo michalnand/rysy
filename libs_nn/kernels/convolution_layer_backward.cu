b42be72b8c91be1eda5954c190ff8c1fff9bcffe
michalnand <michal.nand@gmail.com>
2019-04-23 06:18:29 +0200

    faster net

--- libs_nn/kernels/convolution_layer_backward.cu
+++ libs_nn/kernels/convolution_layer_backward.cu
@@ -50,40 +50,40 @@ void cuda_convolution_layer_weights_gradient(   float *w_grad,
                                                 sGeometry input_geometry,
                                                 sGeometry kernel_geometry )
 {
-  unsigned int kx  = threadIdx.x + blockIdx.x*blockDim.x;
-  unsigned int ky  = threadIdx.y + blockIdx.y*blockDim.y;
-  unsigned int kd  = threadIdx.z + blockIdx.z*blockDim.z;
+    unsigned int kx  = threadIdx.x + blockIdx.x*blockDim.x;
+    unsigned int ky  = threadIdx.y + blockIdx.y*blockDim.y;
+    unsigned int kd  = threadIdx.z + blockIdx.z*blockDim.z;
 
-  if (kd < kernel_geometry.d)
-  if (ky < kernel_geometry.h)
-  if (kx < kernel_geometry.w)
-  {
-      unsigned int kw_half      = (kernel_geometry.w - 1)/2;
-      unsigned int kh_half      = (kernel_geometry.h - 1)/2;
-      unsigned int input_size_y = input_geometry.h - kh_half;
-      unsigned int input_size_x = input_geometry.w - kw_half;
+    if (kd < kernel_geometry.d)
+    if (ky < kernel_geometry.h)
+    if (kx < kernel_geometry.w)
+    {
+          unsigned int kw_half      = (kernel_geometry.w - 1)/2;
+          unsigned int kh_half      = (kernel_geometry.h - 1)/2;
+          unsigned int input_size_y = input_geometry.h - kh_half;
+          unsigned int input_size_x = input_geometry.w - kw_half;
 
-      unsigned int filter = kd/input_geometry.d;
-      unsigned int ch     = kd%input_geometry.d;
-      unsigned int w_ofs  = (kd*kernel_geometry.h + ky)*kernel_geometry.w + kx;
+          unsigned int filter = kd/input_geometry.d;
+          unsigned int ch     = kd%input_geometry.d;
+          unsigned int w_ofs  = (kd*kernel_geometry.h + ky)*kernel_geometry.w + kx;
 
-      float w_dif = 0.0;
+          float w_dif = 0.0;
 
-      for (unsigned int y = 0; y < input_size_y; y++)
-      {
-        unsigned int input_idx  = (ch*input_geometry.h + y + ky)*input_geometry.w + kx;
-        unsigned int error_idx  = (filter*error_geometry.h + y + kh_half)*error_geometry.w + kw_half;
+          for (unsigned int y = 0; y < input_size_y; y++)
+          {
+              unsigned int input_idx  = (ch*input_geometry.h + y + ky)*input_geometry.w + kx;
+              unsigned int error_idx  = (filter*error_geometry.h + y + kh_half)*error_geometry.w + kw_half;
 
-        for (unsigned int x = 0; x < input_size_x; x++)
-        {
-          w_dif+= error[error_idx]*input[input_idx];
-          error_idx++;
-          input_idx++;
-        }
+              for (unsigned int x = 0; x < input_size_x; x++)
+              {
+                  w_dif+= error[error_idx]*input[input_idx];
+                  error_idx++;
+                  input_idx++;
+              }
+          }
+
+          w_grad[w_ofs]+= w_dif;
       }
-
-      w_grad[w_ofs]+= w_dif;
-  }
 }
 
 
@@ -278,8 +278,8 @@ void cpu_convolution_back_kernel( float *error,
 }
 
 
+//OLD implementations - SLOW
 /*
-OLD implementations - SLOW
 template<const unsigned int kernel_size>
 __global__
 void cuda_convolution_back_kernel(  float *error,
@@ -400,12 +400,12 @@ void cuda_convolution_back_kernel_3(    float *error,
     unsigned int channel = threadIdx.z + blockIdx.z*blockDim.z;
 
 
-
     if ((channel < channels_count) && (y < height) && (x < width))
     {
-        unsigned int kernel_size    = 3;
         unsigned int width_         = width - 1;
         unsigned int height_        = height - 1;
+        unsigned int kernel_size    = 3;
+
         float sum = 0.0;
 
         for (unsigned int kernel = 0; kernel < kernels_count; kernel++)
@@ -448,6 +448,91 @@ void cuda_convolution_back_kernel_3(    float *error,
         unsigned int error_back_idx = (channel*height + y)*width + x;
         error_back[error_back_idx]  = sum;
     }
+
+
+
+    /*
+    if (channel < channels_count)
+    {
+        if ((y < height-1) && (x < width-1) && (y > 0) && (x > 0))
+        {
+            unsigned int width_         = width - 1;
+            unsigned int height_        = height - 1;
+            unsigned int kernel_size    = 3;
+
+            float sum = 0.0;
+
+            for (unsigned int kernel = 0; kernel < kernels_count; kernel++)
+            {
+                unsigned int w_idx        = (kernel*channels_count + channel)*kernel_size*kernel_size;
+                unsigned int error_offset = (kernel*height + y + 0)*width + x + 0;
+
+                sum+= w[w_idx]*error[error_offset + 1*width + 1]; w_idx++;
+                sum+= w[w_idx]*error[error_offset + 1*width + 0]; w_idx++;
+                sum+= w[w_idx]*error[error_offset + 1*width - 1]; w_idx++;
+
+                sum+= w[w_idx]*error[error_offset + 0*width + 1]; w_idx++;
+                sum+= w[w_idx]*error[error_offset + 0*width + 0]; w_idx++;
+                sum+= w[w_idx]*error[error_offset + 0*width - 1]; w_idx++;
+
+                sum+= w[w_idx]*error[error_offset - 1*width + 1]; w_idx++;
+                sum+= w[w_idx]*error[error_offset - 1*width + 0]; w_idx++;
+                sum+= w[w_idx]*error[error_offset - 1*width - 1]; w_idx++;
+            }
+
+            unsigned int error_back_idx = (channel*height + y)*width + x;
+            error_back[error_back_idx]  = sum;
+        }
+
+        if (((y == height-1)||(y == 0)) && ((x == width-1)||(x == 0)) )
+        {
+            unsigned int width_         = width - 1;
+            unsigned int height_        = height - 1;
+            unsigned int kernel_size    = 3;
+
+            float sum = 0.0;
+
+
+            for (unsigned int kernel = 0; kernel < kernels_count; kernel++)
+            {
+                unsigned int w_idx        = (kernel*channels_count + channel)*kernel_size*kernel_size;
+                unsigned int error_offset = (kernel*height + y + 0)*width + x + 0;
+
+                if ((x < width_) && (y < height_))
+                    sum+= w[w_idx]*error[error_offset + 1*width + 1]; w_idx++;
+
+                if (y < height_)
+                    sum+= w[w_idx]*error[error_offset + 1*width + 0]; w_idx++;
+
+                if ((x > 0) && (y < height_))
+                    sum+= w[w_idx]*error[error_offset + 1*width - 1]; w_idx++;
+
+
+
+                if (x < width_)
+                    sum+= w[w_idx]*error[error_offset + 0*width + 1]; w_idx++;
+
+                if (true)
+                    sum+= w[w_idx]*error[error_offset + 0*width + 0]; w_idx++;
+
+                if (x > 0)
+                    sum+= w[w_idx]*error[error_offset + 0*width - 1]; w_idx++;
+
+
+
+                if ((x < width_) && (y > 0))
+                    sum+= w[w_idx]*error[error_offset - 1*width + 1]; w_idx++;
+
+                if (y > 0)
+                    sum+= w[w_idx]*error[error_offset - 1*width + 0]; w_idx++;
+
+                if ((x > 0) && (y > 0))
+                    sum+= w[w_idx]*error[error_offset - 1*width - 1]; w_idx++;
+            }
+        }
+    }
+    */
+
 }
 
 
@@ -506,8 +591,6 @@ void cuda_convolution_back_kernel_any_size(   float *error,
 
 void convolution_layer_backward( Tensor &error_back, Tensor &input, Tensor &error, Tensor &w)
 {
-    error_back.clear();
-
     unsigned int kernel_width = w.w();
     unsigned int kernel_height = w.h();
 
@@ -515,7 +598,7 @@ void convolution_layer_backward( Tensor &error_back, Tensor &input, Tensor &erro
 
     if ((kernel_width == 1) && (kernel_height == 1))
     {
-        dim3 block(8, 8, 1);
+        dim3 block(8, 8, 4);
         dim3 grid( (error_back.w()      + block.x + 1)/block.x,
                    (error_back.h()      + block.y + 1)/block.y,
                    (error_back.d()      + block.z + 1)/block.z );
@@ -531,7 +614,7 @@ void convolution_layer_backward( Tensor &error_back, Tensor &input, Tensor &erro
     }
     else if ((kernel_width == 3) && (kernel_height == 3))
     {
-        dim3 block(8, 8, 1);
+        dim3 block(8, 8, 8);
         dim3 grid( (error_back.w()      + block.x + 1)/block.x,
                    (error_back.h()      + block.y + 1)/block.y,
                    (error_back.d()      + block.z + 1)/block.z );
@@ -545,7 +628,9 @@ void convolution_layer_backward( Tensor &error_back, Tensor &input, Tensor &erro
                                                             error_back.d(),
                                                             error.d() );
 
-                                                            /*
+/*
+        error_back.clear();
+
       dim3 block(4, 4, 16);
       dim3 grid( (error_back.w()      + block.x + 1)/block.x,
                  (error_back.h()      + block.y + 1)/block.y,
@@ -559,10 +644,12 @@ void convolution_layer_backward( Tensor &error_back, Tensor &input, Tensor &erro
                                                         error.h(),
                                                         error_back.d(),
                                                         error.d() );
-                                                        */
+*/
     }
     else
     {
+        error_back.clear();
+
         dim3 block(4, 4, 16);
         dim3 grid( (error_back.w()      + block.x + 1)/block.x,
                    (error_back.h()      + block.y + 1)/block.y,
