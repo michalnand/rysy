3382000e02847cefa43d694afbf58cf21e955f1a
michalnand <michal.nand@gmail.com>
2019-05-16 12:34:33 +0200

    average pooling

--- libs_nn/kernels/average_pooling_layer.cu
+++ libs_nn/kernels/average_pooling_layer.cu
@@ -25,10 +25,13 @@ void cpu_average_pooling_forward_kernel(    float *output,
         unsigned int input_height = output_height*kernel_height;
 
         for (unsigned int ky = 0; ky < kernel_height; ky++)
-        for (unsigned int kx = 0; kx < kernel_width; kx++)
         {
-            unsigned int input_idx = (ch*input_height + y_ + ky)*input_width + x_ + kx;
-            result+= input[input_idx];
+            unsigned int input_idx = (ch*input_height + y_ + ky)*input_width + x_;
+            for (unsigned int kx = 0; kx < kernel_width; kx++)
+            {
+                result+= input[input_idx];
+                input_idx++;
+            }
         }
 
         unsigned int output_idx = (ch*output_height + y)*output_width + x;
@@ -64,10 +67,13 @@ void cuda_average_pooling_forward_kernel( float *output,
         unsigned int input_height = output_height*kernel_height;
 
         for (unsigned int ky = 0; ky < kernel_height; ky++)
-        for (unsigned int kx = 0; kx < kernel_width; kx++)
         {
-            unsigned int input_idx = (ch*input_height + y_ + ky)*input_width + x_ + kx;
-            result+= input[input_idx];
+            unsigned int input_idx = (ch*input_height + y_ + ky)*input_width + x_;
+            for (unsigned int kx = 0; kx < kernel_width; kx++)
+            {
+                result+= input[input_idx];
+                input_idx++;
+            }
         }
 
         unsigned int output_idx = (ch*output_height + y)*output_width + x;
@@ -82,7 +88,7 @@ void average_pooling_layer_forward(Tensor &output, Tensor &input)
 
     #ifdef NETWORK_USE_CUDA
 
-      dim3 block(4, 4, 8);
+      dim3 block(2, 2, 16);
       dim3 grid( (output.w() + block.x + 1)/block.x,
                  (output.h() + block.y + 1)/block.y,
                  (output.d() + block.z + 1)/block.z);
@@ -144,11 +150,14 @@ void cpu_average_pooling_backward_kernel(   float *error_back,
         unsigned int error_back_width   = error_width*kernel_width;
 
         for (unsigned int ky = 0; ky < kernel_height; ky++)
+        {
+            unsigned int error_back_idx = (ch*error_back_height + y*kernel_height + ky)*error_back_width + x*kernel_width;
             for (unsigned int kx = 0; kx < kernel_width; kx++)
             {
-                unsigned int error_back_idx = (ch*error_back_height + y*kernel_height + ky)*error_back_width + x*kernel_width + kx;
                 error_back[error_back_idx] = result;
+                error_back_idx++;
             }
+        }
     }
 }
 
@@ -178,11 +187,14 @@ void cuda_average_pooling_backward_kernel(  float *error_back,
         unsigned int error_back_width   = error_width*kernel_width;
 
         for (unsigned int ky = 0; ky < kernel_height; ky++)
+        {
+            unsigned int error_back_idx = (ch*error_back_height + y*kernel_height + ky)*error_back_width + x*kernel_width;
             for (unsigned int kx = 0; kx < kernel_width; kx++)
             {
-                unsigned int error_back_idx = (ch*error_back_height + y*kernel_height + ky)*error_back_width + x*kernel_width + kx;
                 error_back[error_back_idx] = result;
+                error_back_idx++;
             }
+        }
     }
 }
 
@@ -196,7 +208,7 @@ void average_pooling_layer_backward(Tensor &error_back, Tensor &error)
 
   #ifdef NETWORK_USE_CUDA
 
-    dim3 block(4, 4, 8);
+    dim3 block(2, 2, 16);
     dim3 grid(  (error.w()+ block.x + 1)/block.x,
                 (error.h()+ block.y + 1)/block.y,
                 (error.d()+ block.z + 1)/block.z);
